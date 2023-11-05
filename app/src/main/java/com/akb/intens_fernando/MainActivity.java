f11749fee38596fdf116e6f14d6b56af2b5e771e
Fernando <fernandoaja091@gmail.com>
2023-11-05 23:38:43 +0700

    Initial commit

---
+++ app/src/main/java/com/akb/intens_fernando/MainActivity.java
@@ -0,0 +1,21 @@
+package com.akb.intens_fernando;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void pindah (View View) {
+        Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+        startActivity(intent);
+
+
+    }
+}
